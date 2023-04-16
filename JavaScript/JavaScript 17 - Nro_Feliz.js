86223c9c4dc5cc43fac0ad49d5c47b599c84c5fd
Aylen0042 <aylendenisedominguez@gmail.com>
2023-04-16 15:22:28 -0300

    Numero feliz

---
+++ JavaScript/JavaScript 17 - Nro_Feliz.js
@@ -0,0 +1,31 @@
+/*
+    CONSIGNA: Hacer un programa que muestre mediante una función llamada function EsFeliz (n) muestre el listado de los números felices
+              hasta el número ingresado. 
+*/
+
+let num = prompt("Ingrese un numero", '');
+
+if(esFeliz(num) == 1)
+    document.write("El " + num + " es un numero feliz");
+
+function esFeliz(num)
+{
+        let suma=0;
+
+        if(num == 1)
+            return num;
+
+        for(let i=0; i<num.length; i++)
+        {
+            cuadrado = num[i] * num[i];
+            document.write(num[i] + "->" + cuadrado + "<br>");
+            suma = suma + cuadrado;
+        }
+        document.write("Suma: " + suma + "<br>");
+
+        if(suma == 1)
+            return suma;
+        
+        num = suma.toString();
+        return esFeliz(num);
+}
